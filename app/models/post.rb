fcfb4e3158819d811003a9bf6ed994d3793f549b
kame-888 <rioikegame1111@gmail.com>
2026-09-19 22:43:19 +0900

    railsバリエーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 20}
+  validates :start_date, presence: true
+  validates :end_date, presence: true, comparison: {greater_than: :start_date}
+  validates :body, length: {maximum: 500}
 end
